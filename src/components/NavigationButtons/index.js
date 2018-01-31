1b392b95ae4d6885b278c70538748a7a0c43cce7
Maximiliano Forlenza <forlenzamaximiliano@gmail.com>
2018-01-31 15:46:11 -0300

    feat(navigationButtons): add NavigatorButtons component

---
+++ src/components/NavigationButtons/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Button, Col, Row} from '@indec/react-native-commons';
+
+import styles from './styles';
+
+const NavigatorButtons = ({onBack, submitButtonText, onSubmit}) => (
+    <Row>
+        <Col>
+            <Button
+                title="Anterior"
+                onPress={onBack}
+                rounded
+                buttonStyle={styles.button}
+            />
+        </Col>
+        <Col>
+            <Button
+                title={submitButtonText}
+                onPress={onSubmit}
+                rounded
+                buttonStyle={styles.button}
+            />
+        </Col>
+    </Row>
+);
+
+NavigatorButtons.propTypes = {
+    onBack: PropTypes.func.isRequired,
+    onSubmit: PropTypes.func.isRequired,
+    submitButtonText: PropTypes.string.isRequired
+};
+
+export default NavigatorButtons;

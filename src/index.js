c45ccf14998b29d4d49df50cf141704bcb50bf99
maximilianoforlenza <forlenzamaximiliano@gmail.com>
2018-01-31 13:45:15 -0300

    feat(dwellingResponse): add DwellingResponse component (#15)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import AddressCard from './components/AddressCard';
 import AddressesList from './components/AddressesList';
 import AreasList from './components/AreasList';
+import DwellingResponse from './components/DwellingResponse';
 import Sync from './components/Sync';
 
 export {AddressCard};
 export {AddressesList};
 export {AreasList};
+export {DwellingResponse};
 export {Sync};

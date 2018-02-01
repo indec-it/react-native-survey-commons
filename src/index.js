67d8e011f6df90189d427156f21c515c0401f4c5
Maximiliano Forlenza <forlenzamaximiliano@gmail.com>
2018-02-01 13:56:53 -0300

    feat(householdsList): add HouseholdsList component

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import AddressCard from './components/AddressCard';
 import AddressesList from './components/AddressesList';
 import AreasList from './components/AreasList';
 import DwellingResponse from './components/DwellingResponse';
+import HouseholdsList from './components/HouseholdsList';
 import Sync from './components/Sync';
 
 export {AddressCard};
 export {AddressesList};
 export {AreasList};
 export {DwellingResponse};
+export {HouseholdsList};
 export {Sync};

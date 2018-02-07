9e1901a3399333994674593930ca6de8c1e0e7e8
guillermodv <guillermodj1@yahoo.com.ar>
2018-02-07 18:19:34 -0300

    fix(form): fix using form-builder ComponentsRegistry (#34)

--- src/components/FormBuilder/index.js
+++ src/components/FormBuilder/index.js
@@ -2,36 +2,38 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {ScrollView} from 'react-native';
 import {Row} from '@indec/react-native-commons';
-import {ComponentsMapper} from '@indec/react-native-form-builder';
+import {ComponentsRegistry} from '@indec/react-native-form-builder';
 import {filter} from 'lodash';
 
 import {canDrawQuestion} from '../../util';
 
-const FormBuilder = ({chapter, onChange, rows}) => {
-    const mapper = new ComponentsMapper();
-    return (
-        <ScrollView>
-            {rows.map(row => (
-                <Row key={row.id}>
-                    {filter(
-                        row.questions,
-                        question => canDrawQuestion(question, chapter)
-                    ).map(question => {
-                        const QuestionComponent = mapper.getComponent(question.type);
+const registry = new ComponentsRegistry();
+
+const FormBuilder = ({chapter, onChange, rows}) => (
+    <ScrollView>
+        {rows.map(row => (
+            <Row key={row.id}>
+                {filter(
+                    row.questions,
+                    question => canDrawQuestion(question, chapter)
+                )
+                    .map(question => {
+                        const QuestionComponent = registry.get(question.type);
                         return (
                             <QuestionComponent
                                 key={question.number}
                                 question={question}
+                                section={chapter}
                                 answer={chapter[question.name]}
                                 onChange={answer => onChange(answer)}
                             />
                         );
-                    })}
-                </Row>
-            ))}
-        </ScrollView>
-    );
-};
+                    })
+                }
+            </Row>
+        ))}
+    </ScrollView>
+);
 
 FormBuilder.propTypes = {
     rows: PropTypes.shape({}).isRequired,

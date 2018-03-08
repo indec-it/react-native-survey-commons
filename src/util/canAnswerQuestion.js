ccbbd9e148d51d68b577c6f7047afcd2f669f398
maximilianoforlenza <forlenzamaximiliano@gmail.com>
2018-03-08 09:58:32 -0300

    feat(utils): add AND conditional for canAnswerQuestion (#91)

--- src/util/canAnswerQuestion.js
+++ src/util/canAnswerQuestion.js
@@ -1,6 +1,17 @@
 import {operators} from '@indec/react-native-form-builder';
 import lang from 'lodash/lang';
-import {some, isNil} from 'lodash';
+import {every, isArray, isNil, some} from 'lodash';
+
+const canShowQuestion = (parent, chapter) => {
+    switch (parent.type) {
+        case operators.EXISTS:
+            return !isNil(chapter[parent.id]) === parent.value;
+        case operators.NOT_EQUALS:
+            return !lang.eq(chapter[parent.id], parent.value);
+        default:
+            return lang[parent.type](chapter[parent.id], parent.value);
+    }
+};
 
 const canAnswerQuestion = ({parents}, chapter) => {
     if (!parents) {
@@ -9,14 +20,10 @@ const canAnswerQuestion = ({parents}, chapter) => {
     return some(
         parents,
         parent => {
-            switch (parent.type) {
-                case operators.EXISTS:
-                    return !isNil(chapter[parent.id]) === parent.value;
-                case operators.NOT_EQUALS:
-                    return !lang.eq(chapter[parent.id], parent.value);
-                default:
-                    return lang[parent.type](chapter[parent.id], parent.value);
+            if (isArray(parent)) {
+                return every(parent, p => canShowQuestion(p, chapter));
             }
+            return canShowQuestion(parent, chapter);
         }
     );
 };

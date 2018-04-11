f456e3aede19e7dc5c68f9a9cd217e3ff5485542
Juancoot <juan_manuel_lopez_1996@hotmail.com>
2018-04-11 13:39:50 -0300

    feat(memberEditor): add member name in MemberEditor component (#142)

--- src/components/MemberEditor/styles.js
+++ src/components/MemberEditor/styles.js
@@ -3,5 +3,17 @@ import {StyleSheet} from 'react-native';
 export default StyleSheet.create({
     actionButtons: {
         marginTop: 20
+    },
+    nameContainer: {
+        height: 30,
+        paddingRight: 10,
+        paddingLeft: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        backgroundColor: '#ebeff0'
+    },
+    nameText: {
+        fontSize: 14
     }
 });

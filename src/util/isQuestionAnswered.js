6baa5053b6ae548629d4b2ebb3b70303bc999009
Maximiliano Forlenza <forlenzamaximiliano@gmail.com>
2018-04-24 23:08:42 -0300

    fix(util): add isQuestionAnswered for decimal input

--- src/util/isQuestionAnswered.js
+++ src/util/isQuestionAnswered.js
@@ -1,5 +1,5 @@
 import {types} from '@indec/react-native-form-builder';
-import {every, isArray, isEmpty} from 'lodash';
+import {every, isArray, isEmpty, isNumber} from 'lodash';
 
 import canAnswerQuestion from './canAnswerQuestion';
 
@@ -14,10 +14,12 @@ const isQuestionAnswered = (question, section) => {
     switch (question.type) {
         case types.TEXT_WITH_BADGE:
         case types.TITLE:
-        case types.TOTAL:
+        case types.SUM:
             return true;
         case types.RADIO_TABLE:
             return isAnswered(question, section, every(question.questions, q => section[question.name + q.name]));
+        case types.DECIMAL_INPUT:
+            return isNumber(section[question.name]);
         default:
             return isAnswered(question, section, section[question.name]);
     }

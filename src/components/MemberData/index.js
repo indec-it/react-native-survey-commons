64c70869559b6167b5c4924b9d2df625321cbad5
Maximiliano Forlenza <forlenzamaximiliano@gmail.com>
2018-05-16 14:40:27 -0300

    style(memberData): change style memberData

--- src/components/MemberData/index.js
+++ src/components/MemberData/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import PropTypes from 'prop-types';
+import {Col, Row} from '@indec/react-native-commons';
 
 import {Member} from '../../model';
 import {getMemberAge, getMemberName} from '../../util';
@@ -8,9 +9,17 @@ import styles from './styles';
 
 const MemberData = ({member, style}) => (
     <View style={[styles.nameContainer, style.nameContainer]}>
-        <Text style={[styles.nameText, style.nameText]}>
-            {member.order} - {getMemberName(member)} ({getMemberAge(member)} años)
-        </Text>
+        <Row>
+            <Col>
+                <Text>Miembro N°: {member.order}</Text>
+            </Col>
+            <Col>
+                <Text>Nombre: {getMemberName(member)}</Text>
+            </Col>
+            <Col>
+                <Text>Edad: {getMemberAge(member)} años</Text>
+            </Col>
+        </Row>
     </View>
 );
 

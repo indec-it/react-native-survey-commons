a498f03f414dcb952ecb9d89fe45c70e0ffa9b7f
maximilianoforlenza <forlenzamaximiliano@gmail.com>
2018-08-28 09:58:52 -0300

    fix(householdVisitPropTypes): fix instanceOf text (#245)

--- src/util/householdVisitPropTypes.js
+++ src/util/householdVisitPropTypes.js
@@ -4,16 +4,16 @@ export default PropTypes.shape({
     id: PropTypes.number.isRequired,
     order: PropTypes.number.isRequired,
     start: PropTypes.oneOfType([
-        PropTypes.instaceOf(Date),
+        PropTypes.instanceOf(Date),
         PropTypes.string
     ]),
     response: PropTypes.number,
     end: PropTypes.oneOfType([
-        PropTypes.instaceOf(Date),
+        PropTypes.instanceOf(Date),
         PropTypes.string
     ]),
     nextVisit: PropTypes.oneOfType([
-        PropTypes.instaceOf(Date),
+        PropTypes.instanceOf(Date),
         PropTypes.string
     ])
 });

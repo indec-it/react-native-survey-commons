df9d79d3c8dbe5a376001fa634df879a6550e2b3
Axel Navarro <navarroaxel@gmail.com>
2018-09-07 10:35:01 -0300

    fix(packages): upgrade commitlint to v7.1.2
    upgrade react@16.5.0

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,6 +3,8 @@ module.exports = {
         'body-leading-blank': [1, 'always'],
         'footer-leading-blank': [1, 'always'],
         'header-max-length': [2, 'always', 72],
+        'body-max-line-length': [2, 'always', 80],
+        'footer-max-line-length': [2, 'always', 80],
         'scope-case': [2, 'always', 'camel-case'],
         'subject-case': [
             2,

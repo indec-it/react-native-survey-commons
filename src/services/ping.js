12c4d314f216e13cec8be307a2192714f8e34c21
Juan Manuel <jlopez>
2019-09-16 11:58:25 -0300

    fix(netInfo): add react native community net info

--- src/services/ping.js
+++ src/services/ping.js
@@ -1,9 +1,9 @@
-import {NetInfo} from 'react-native';
+import NetInfo from '@react-native-community/netinfo';
 import {http} from '@indec/heimdall/native';
 
 export default class PingService {
     static async isConnected() {
-        return NetInfo.isConnected.fetch();
+        return NetInfo.fetch();
     }
 
     static async ping(endpoint) {
